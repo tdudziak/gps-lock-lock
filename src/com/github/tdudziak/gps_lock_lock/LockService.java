c2b472f31be1152596970aaaf48ce7bf700c793a
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2012-01-04 10:52:07 +0100

    Report remaining time in notification text.

--- src/com/github/tdudziak/gps_lock_lock/LockService.java
+++ src/com/github/tdudziak/gps_lock_lock/LockService.java
@@ -24,75 +24,76 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 
 public class LockService extends Service implements LocationListener {
 
     private final String TAG = "LockService";
-    private final long GPS_MIN_TIME = 20000;
-    private final long LOCK_LOCK_TIME = 15 * 60 * 60 * 1000;
+    private final long GPS_MIN_TIME = 0; // 20000;
+    private final long LOCK_LOCK_MINUTES = 5;
     private final int NOTIFICATION_ID = 1;
 
-    private boolean mIsActive = false;
+    private boolean mIsActive = false; // TODO: Get rid of this field.
+    private long mStartTime;
+    private long mLastFixTime = 0;
     private Notification mNotification;
     private PendingIntent mNotificationIntent;
     private LocationManager mLocationManager;
     private NotificationManager mNotificationManager;
+    private RefreshHandler mHandler;
 
-    private void setFixState(boolean has_fix) {
-        assert mNotification != null;
-        CharSequence title, text;
+    private class RefreshHandler extends Handler {
+        private final int WHAT = 0;
 
-        if (has_fix) {
-            title = getText(R.string.notification_title_available);
-            text = getText(R.string.notification_text_available);
-        } else {
-            title = getText(R.string.notification_title_unavailable);
-            text = getText(R.string.notification_text_unavailable);
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            redrawUI();
         }
 
-        mNotification.setLatestEventInfo(getApplicationContext(), title, text,
-                mNotificationIntent);
-        startForeground(NOTIFICATION_ID, mNotification);
-    }
+        public void redrawUI() {
+            if(!mIsActive) return;
 
-    private void showNotification() {
-        int icon = android.R.drawable.stat_notify_sync_noanim; // FIXME
-        CharSequence ticker = getText(R.string.notification_ticker);
+            long minutes = (System.currentTimeMillis() - mStartTime)/(1000*60);
+            long remaining = LOCK_LOCK_MINUTES - minutes;
+            long last_min = (System.currentTimeMillis() - mLastFixTime)/(1000*60);
 
-        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent notificationIntent = new Intent(this, SettingsActivity.class);
-        mNotificationIntent = PendingIntent.getActivity(this, 0,
-                notificationIntent, 0);
-        mNotification = new Notification(icon, ticker,
-                System.currentTimeMillis());
+            Resources res = getResources();
+            CharSequence title, text;
 
-        setFixState(false);
-    }
+            if(remaining <= 0) stopSelf();
 
-    private void startListening() {
-        mLocationManager = (LocationManager) this
-                .getSystemService(Context.LOCATION_SERVICE);
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                GPS_MIN_TIME, 0, this);
-    }
+            if(mLastFixTime <= 0) {
+                title = res.getString(R.string.notification_title_nofix);
+            } else if(last_min > 0) {
+                title = String.format(res.getString(R.string.notification_title), last_min);
+            } else {
+                title = res.getString(R.string.notification_title_1minfix);
+            }
 
-    private void stopEverything() {
-        Log.i(TAG, "Disabling lock-locking");
-        mLocationManager.removeUpdates(this);
-        stopForeground(true);
-        mIsActive = false;
+            text = String.format(res.getString(R.string.notification_text), remaining);
+
+            mNotification.setLatestEventInfo(getApplicationContext(), title, text, mNotificationIntent);
+            mNotificationManager.notify(NOTIFICATION_ID, mNotification);
+
+            removeMessages(WHAT);
+            sendEmptyMessageDelayed(WHAT, 1000);
+        }
     }
 
     @Override
     public void onDestroy() {
-        stopEverything();
+        mLocationManager.removeUpdates(this);
+        stopForeground(true);
+        mIsActive = false;
         Log.i(TAG, "Shutting down");
     }
 
@@ -105,25 +106,39 @@ public class LockService extends Service implements LocationListener {
             mIsActive = true;
         }
 
-        showNotification();
-        startListening();
-        Log.i(TAG, "Service started");
+        mStartTime = System.currentTimeMillis();
 
+        // Init UI.
+        int icon = android.R.drawable.stat_notify_sync_noanim; // FIXME
+        CharSequence ticker = getText(R.string.notification_ticker);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent notificationIntent = new Intent(this, SettingsActivity.class);
+        mNotificationIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        mNotification = new Notification(icon, ticker, mStartTime);
+
+        // Start periodical UI updates.
+        mHandler = new RefreshHandler();
+        mHandler.redrawUI();
+        startForeground(NOTIFICATION_ID, mNotification);
+
+        // Setup GPS listening.
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, GPS_MIN_TIME, 0, this);
+
+        Log.i(TAG, "Service started");
         return START_STICKY;
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        // TODO Auto-generated method stub
         Log.v(TAG, "onLocationChanged()");
-        setFixState(true);
+        mLastFixTime = System.currentTimeMillis();
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        // GPS explicitly turned off. The user obviously does not want a GPS
-        // fix.
-        stopEverything();
+        // GPS explicitly turned off. The user obviously does not want a GPS fix.
+        stopSelf();
     }
 
     @Override
@@ -134,14 +149,8 @@ public class LockService extends Service implements LocationListener {
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
         Log.v(TAG, "onStatusChanged(); status=" + status);
-
-        if (status == LocationProvider.AVAILABLE) {
-            setFixState(true);
-        } else {
-            setFixState(false);
-        }
+        mHandler.redrawUI();
     }
 
     @Override
